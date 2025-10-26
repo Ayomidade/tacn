8df24e9179bf1d00a7f7c3b013687a03aa9311c5
myk3l <ayomikesam@gmail.com>
2025-10-26 16:51:59 +0100

    imported logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./theme.css";
 import { FaBars } from "react-icons/fa";
 import { useScroll } from "../context/ScrollContext";
-import logo from"src/assets/1000092052.png"
+import logo from "../assets/1000092052.png";
 
 // import "./navbar.css";
 
